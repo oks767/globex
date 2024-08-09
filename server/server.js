88c84215089a8f5f33a5331e2e57c25864451e6e
Oksana Aleksandrova <102592873+oks767@users.noreply.github.com>
2024-08-09 19:31:17 +0300

    add bd, add lib pg and create pool

--- server/server.js
+++ server/server.js
@@ -1,98 +1,26 @@
-// const express = require('express')
-// const mysql = require('mysql')
-// const cors = require('cors')
+const { Pool } = require('pg');
 
-// const app = express()
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'websoft',
+  password: 'root',
+  port: 5432,
+});
 
-// app.use(cors())
+const query = (text, params) => pool.query(text, params);
 
-// const db = mysql.createConnection({
- 
-//   host: 'localhost',
-//   user: 'KOMPUTER\oksana',
-//   password: '',
-//   database: 'websoft',
-// })
-// db.connect()
-// app.get('/', (req, res) => {
-  
-//   db.query(`USE websoft
-//             GO
-//             SELECT * FROM data`, function(error, results, fields) {
-//     if (results) {
-//       res.send(results)
-//     } else {
-//       console.log(error);
-//     }
-//   })
-// })
-  
+async function testConnection() {
+  try {
+    const result = await query("SELECT * FROM users");
+    console.log(result, result.rows[0].now);
+  } catch (err) {
+    console.error("Error connecting to the database", err);
+  } finally {
+    await pool.end();
+  }
+}
 
-// app.listen(3306, () => {
-//   console.log('listening');
-// })
-var Connection = require('tedious').Connection;  
-    var config = {  
-        server: 'localhost',  //update me
-        authentication: {
-            type: 'default',
-            
-            options: {
-                userName: 'root', //update me
-                password: 'root',  //update me 
-            }
-        },
-        options: {
-            // If you are on Microsoft Azure, you need encryption:
-            encrypt: true,
-            database: 'websoft',  //update me
-            trustServerCertificate: true,
-        }
-    };  
-    var connection = new Connection(config);  
-    connection.on('connect', function(err) {  
-        // If no error, then good to proceed.
-        if (err) {
-            console.log(err);
-        } else {
-            executeStatement()
-        }
-        // console.log("Connected");  
-    });
-    
-    connection.connect();
-    var Request = require('tedious').Request;  
-    var TYPES = require('tedious').TYPES;  
-  
-    function executeStatement() {  
-        var request = new Request("SELECT * Table_1;", function(err, result) {  
-        if (err) {  
-            console.log(err);} 
-            else {
-                console.log(result);
-            } 
-        });  
-        var result = "";  
-        request.on('row', function(columns) {  
-            columns.forEach(function(column) {  
-              if (column.value === null) {  
-                console.log('NULL');  
-              } else {  
-                result+= column.value + " ";  
-              }  
-            });  
-            console.log(result);  
-            result ="";  
-        });  
-  
-        request.on('done', function(rowCount, more) {  
-        console.log(rowCount + ' rows returned');  
-        });  
-        
-        // Close the connection after the final event emitted by the request, after the callback passes
-        request.on("requestCompleted", function (rowCount, more) {
-            connection.close();
-        });
-        connection.execSql(request);  
-    } 
+testConnection();
+
     
